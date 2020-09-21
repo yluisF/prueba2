25fe44ffdc37d728ef5779bbd1ba51c8c7969759
yluisF <yluisfernando870@gmail.com>
2020-09-21 16:33:16 -0500

    Subiendo el Proyecto base

---
+++ app/Http/Controllers/CategoriesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Category;
+
+class CategoriesController extends Controller
+{
+    public function index(){
+
+        $categories = Category::all();
+        return view('welcome', ['categories' => $categories]);
+    }
+}
